4f98f7cc51db0ab0c3901f6675778585187a3c43
Amit Chaudhary <amit2cha@gmail.com>
2020-01-06 18:52:58 +0530

    play.js

--- basics/play.js
+++ basics/play.js
@@ -1,9 +1,16 @@
 var name = 'Amit';
-var age = 23;
+var age = 24;
 var hasHobbies = 'True';
 
-function summeriseUser(username, userage, userhasHobbies) {
-    return ('Name is ' + username + ', age is ' + userage + ' , User have Hobbies ' + userhasHobbies);
-}
+let summeriseUser = (username, userage, userhasHobbies) => {
+    return (
+        'Name is ' +
+        username +
+        ', age is ' +
+        userage +
+        ' , User have Hobbies ' +
+        userhasHobbies
+    );
+};
 
 console.log(summeriseUser(name, age, hasHobbies));
